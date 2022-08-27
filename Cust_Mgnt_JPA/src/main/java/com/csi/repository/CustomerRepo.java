99b761f13100a8ac75c2b441c62a26440048b279
TejuMahajan <mahtejaswini101@gamil.com>
2022-08-27 17:22:00 +0530

    doneeeee

---
+++ Cust_Mgnt_JPA/src/main/java/com/csi/repository/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.csi.repository;
+
+import com.csi.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
+
+    //custom methods
+}
